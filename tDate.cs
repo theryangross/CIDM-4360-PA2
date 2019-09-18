8bfccc7634ecfff066f7613b7d092f00fd0a9eb9
Ryan Gross <ryangross123@icloud.com>
2019-09-18 18:14:19 -0500

    finished second constructor

--- tDate.cs
+++ tDate.cs
@@ -26,7 +26,26 @@ class tDate
         }
     }
 
-    
+    public tDate(string Date)
+    {
+        String[] DateObject = Date.Split("/");
+        int Month = int.Parse(DateObject[0]);
+        int Day = int.Parse(DateObject[1]);
+        int Year = int.Parse(DateObject[2]);
+
+        if ((0 < Month && Month <= 12) && (0 < Day && Day <= 31) && (Year>0))
+        {
+            Console.WriteLine("{0}/{1}/{2}", Month, Day, Year);
+        }
+        else
+        {
+            String[] eDate = earliestDate.Split("/");
+            int earlyMonth = int.Parse(eDate[0]);
+            int earlyDay = int.Parse(eDate[1]);
+            int earlyYear = int.Parse(eDate[2]);
+            Console.WriteLine("{0}/{1}/{2}", earlyMonth, earlyDay, earlyYear);
+        }
+    }
 
     public void showDate ()
     {
